507661829d3bc234da93f6b0e8c8834d0a907d6b
Slain505 <vladyslav.yefimenko@protonmail.com>
2023-01-22 21:31:58 +0100

    First playable version of the game

--- Assets/Scripts/Fruit.cs
+++ Assets/Scripts/Fruit.cs
@@ -1,10 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class Fruit : MonoBehaviour
+namespace Snake
 {
-    private List<GameObject> fruitBody = new List<GameObject>();
-
-
+    public class Fruit : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (GameObject.FindWithTag("apple"))
+            {
+                Vector3 randomSpawnPosition = new Vector3(Random.Range(-13, 11), 1.2f, Random.Range(-24, -1));
+                transform.position = randomSpawnPosition;
+                PlayerController.pc.SnakeAddParts();
+                Debug.Log("UwU +1");
+            }
+        }
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,58 +6,73 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using Random = UnityEngine.Random;
 
-public class PlayerController : MonoBehaviour
+namespace Snake
 {
-    [SerializeField] private float steerSpeed = 180;
-    [SerializeField] private float speed = 5;
-    [SerializeField] private int gap = 15;
-    [SerializeField] private float bodySpeed = 5;
-    [SerializeField] private GameObject bodyPref;
-    [SerializeField] private GameObject fruit;
-
-    private List<GameObject> bodyParts = new List<GameObject>();
-    private List<Vector3> MoveHistory = new List<Vector3>();
-
-    private void Start()
-    {
-        Debug.Log("Game Start!");
-        SnakeAddParts();
-        SnakeAddParts();
-        SnakeAddParts();
-        SnakeAddParts();
-    }
-
-    private void Update()
-    {
-        transform.position += transform.forward * speed * Time.deltaTime;
-        float steerDirection = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up * steerSpeed * steerDirection * Time.deltaTime);
-        
-        MoveHistory.Insert(0,transform.position);
-        
-        int index = 0;
-        foreach (var body in bodyParts)
-        {
-            Vector3 point = MoveHistory[Mathf.Min(index * gap, MoveHistory.Count - 1)];
-            Vector3 moveDirection = point - body.transform.position;
-            body.transform.position += moveDirection * bodySpeed * Time.deltaTime;
-            body.transform.LookAt(point);
-            index++;
-        }
-    }
-
-    public void SnakeAddParts()
-    {
-        GameObject body = Instantiate(bodyPref);
-        bodyParts.Add(body);
-    }
     
-    private void OnTriggerEnter(Collider other)
+    public class PlayerController : MonoBehaviour
     {
-        if (GameObject.FindWithTag("apple"))
+        [SerializeField] private float steerSpeed = 180;
+        [SerializeField] private float speed = 5;
+        [SerializeField] private int gap = 15;
+        [SerializeField] private float bodySpeed = 5;
+        [SerializeField] private GameObject bodyPref;
+        [SerializeField] private GameObject fruit;
+        public static PlayerController pc;
+
+        private List<GameObject> bodyParts = new List<GameObject>();
+        private List<Vector3> MoveHistory = new List<Vector3>();
+
+        private void Awake()
         {
-            GameObject.FindWithTag("apple").SetActive(false);
-            Debug.Log("UwU +1");
+            pc = this;
+        }
+
+        private void Start()
+        {
+            Debug.Log("Game Start!");
+            SnakeAddParts();
+            SnakeAddParts();
+            SnakeAddParts();
+            SnakeAddParts();
+        }
+
+        private void Update()
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+            float steerDirection = Input.GetAxis("Horizontal");
+            transform.Rotate(Vector3.up * steerSpeed * steerDirection * Time.deltaTime);
+
+            MoveHistory.Insert(0, transform.position);
+
+            int index = 0;
+            foreach (var body in bodyParts)
+            {
+                Vector3 point = MoveHistory[Mathf.Min(index * gap, MoveHistory.Count - 1)];
+                Vector3 moveDirection = point - body.transform.position;
+                body.transform.position += moveDirection * bodySpeed * Time.deltaTime;
+                body.transform.LookAt(point);
+                index++;
+            }
+        }
+
+        public void SnakeAddParts()
+        {
+            GameObject body = Instantiate(bodyPref);
+            bodyParts.Add(body);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            // if (GameObject.FindWithTag("apple"))
+            // {
+            //     GameObject.FindWithTag("apple").SetActive(false);
+            //     Debug.Log("UwU +1");
+            //     Vector3 randomSpawnPosition = new Vector3(Random.Range(-13, 11), 1.2f, Random.Range(-24, -1));
+            //     Instantiate(fruit, randomSpawnPosition, Quaternion.identity);
+            // }
+
+
+
         }
     }
 }
